bf935fa6a7185a191df3f2cd77364638a4451fd9
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-11-20 11:07:20 -0600

    start client project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello World</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
