8679162f2d296a7380e9628393861088693caffc
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-11-25 16:04:29 -0600

    update sql query file

--- db/database.sql
+++ db/database.sql
@@ -1,11 +1,32 @@
-CREATE DATABASE IF NOT EXISTS washroom;
 
+CREATE DATABASE railway;
+
+USE railway;
+     
 CREATE TABLE machine (
-    id INT(10) NOT NULL AUTO_INCREMENT;
-    name VARCHAR(45) DEFAULT NULL,
-    onUse BOOLEAN DEFAULT 0,
-    washing BOOLEAN DEFAULT 0,
-    PRIMARY KEY (id);
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL,
+    cycle_id INT
+);
+  
+CREATE TABLE user (
+    phone VARCHAR(14) PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-INSERT INTO machine VALUES (1, 'Lavadora', 0, 0);
+CREATE TABLE cycle (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    machine_id INT NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status TEXT NOT NULL,
+    startedAt TIMESTAMP,
+    user_phone VARCHAR(14),
+    warning BOOLEAN DEFAULT 0,
+    FOREIGN KEY (machine_id) REFERENCES machine(id),
+    FOREIGN KEY (user_phone) REFERENCES user(phone)
+);
+
+ALTER TABLE machine ADD FOREIGN KEY (cycle_id) REFERENCES cycle(id);
+
+INSERT INTO machine (name) VALUES ('Lavadora');
+INSERT INTO user (phone, name) VALUES ('01010101010', 'Miguel Mendoza');
