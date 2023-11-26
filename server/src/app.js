e0466336c981eb92fb1027c2931374bd61271c3c
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-11-26 11:16:04 -0600

    modify cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,12 @@ import indexRoutes from "./routes/index.routes.js";
 import cors from 'cors'; 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: '*',
+  allowedHeaders: "*",
+}));
+
 app.use(express.json());
 
 app.use(indexRoutes);
