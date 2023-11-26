c20fe3a5cf93c5fe1c9d53e0846f846ff296dcd3
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-11-26 00:27:30 -0600

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
-
+import cors from 'cors'; 
 const app = express();
+
+app.use(cors);
 app.use(express.json());
 
 app.use(indexRoutes);
